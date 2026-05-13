61c0bdf4c91d0bc11b14aebcd6b5687fb5678a0a
BYIRINGIRO-Samuel <byiringirosam08@gmail.com>
2026-05-13 18:44:46 +0100

    Update src/jobs/jobs.module.ts

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { JobsController } from './jobs.controller';
-import { Job, JobSchema } from './job.schema';
+import { Job } from '../entities/job.entity';
+import { Application } from '../entities/application.entity';
+import { Candidate } from '../entities/candidate.entity';
 import { JobsService } from './jobs.service';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Job.name, schema: JobSchema }])],
+  imports: [TypeOrmModule.forFeature([Job, Application, Candidate])],
   controllers: [JobsController],
   providers: [JobsService],
   exports: [JobsService],
 })
-export class JobsModule {}
+export class JobsModule { }
