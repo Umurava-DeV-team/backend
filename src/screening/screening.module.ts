c67f958b7aadeb23d57bf7f3640af373e0b88908
BYIRINGIRO-Samuel <byiringirosam08@gmail.com>
2026-05-13 18:44:47 +0100

    Update src/screening/screening.module.ts

--- src/screening/screening.module.ts
+++ src/screening/screening.module.ts
@@ -1,27 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScreeningController } from './screening.controller';
 import { ScreeningService } from './screening.service';
 import { GeminiService } from './gemini.service';
 import { JobsModule } from '../jobs/jobs.module';
 import { CandidatesModule } from '../candidates/candidates.module';
 import { AssessmentsModule } from '../assessments/assessments.module';
-
 import { ApplicationsModule } from '../applications/applications.module';
 import { ProfileModule } from '../profile/profile.module';
-
-import { MongooseModule } from '@nestjs/mongoose';
-import { ScreeningResultModel, ScreeningResultSchema } from './screening-result.schema';
+import { AuthModule } from '../auth/auth.module';
+import { ScreeningResult } from '../entities/screening-result.entity';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: ScreeningResultModel.name, schema: ScreeningResultSchema }]),
-    JobsModule, 
-    CandidatesModule, 
-    AssessmentsModule, 
-    ApplicationsModule, 
-    ProfileModule
+    TypeOrmModule.forFeature([ScreeningResult]),
+    JobsModule,
+    CandidatesModule,
+    AssessmentsModule,
+    ApplicationsModule,
+    ProfileModule,
+    AuthModule
   ],
   controllers: [ScreeningController],
   providers: [ScreeningService, GeminiService],
 })
-export class ScreeningModule {}
+export class ScreeningModule { }
