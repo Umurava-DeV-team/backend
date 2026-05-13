4ef8ed3764788343e643045223066b3097071871
BYIRINGIRO-Samuel <byiringirosam08@gmail.com>
2026-05-13 18:44:46 +0100

    Update src/notifications/notifications.module.ts

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,14 +1,14 @@
 import { Global, Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { NotificationsGateway } from './notifications.gateway';
 import { NotificationsService } from './notifications.service';
 import { NotificationsController } from './notifications.controller';
-import { Notification, NotificationSchema } from './notification.schema';
+import { Notification } from '../entities/notification.entity';
 
 @Global()
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Notification.name, schema: NotificationSchema }])
+    TypeOrmModule.forFeature([Notification])
   ],
   controllers: [NotificationsController],
   providers: [NotificationsGateway, NotificationsService],
