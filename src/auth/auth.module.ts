320aeee93d0565104ec211c1ed9ea19094c069a9
BYIRINGIRO-Samuel <byiringirosam08@gmail.com>
2026-05-13 18:44:45 +0100

    Update src/auth/auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { JwtStrategy } from './jwt.strategy';
-import { User, UserSchema } from './user.schema';
+import { User } from '../entities/user.entity';
 import { UsersService } from './users.service';
 
 @Module({
   imports: [
     PassportModule,
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    TypeOrmModule.forFeature([User]),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: (config: ConfigService) => ({
-      secret: config.get<string>('JWT_SECRET') ?? 'fallback_secret',
+        secret: config.get<string>('JWT_SECRET') ?? 'fallback_secret',
         signOptions: { expiresIn: '7d' },
       }),
       inject: [ConfigService],
@@ -27,4 +27,4 @@ import { UsersService } from './users.service';
   providers: [AuthService, UsersService, JwtStrategy, JwtAuthGuard],
   exports: [UsersService, JwtAuthGuard],
 })
-export class AuthModule {}
+export class AuthModule { }
