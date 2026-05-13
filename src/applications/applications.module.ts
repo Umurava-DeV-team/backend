e4fca0711c4fac133b501cb96bfbb2cee4dca506
BYIRINGIRO-Samuel <byiringirosam08@gmail.com>
2026-05-13 18:44:45 +0100

    Update src/applications/applications.module.ts

--- src/applications/applications.module.ts
+++ src/applications/applications.module.ts
@@ -1,19 +1,17 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Application, ApplicationSchema } from './application.schema';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Application } from '../entities/application.entity';
 import { ApplicationsService } from './applications.service';
 import { ApplicationsController } from './applications.controller';
 import { JobsModule } from '../jobs/jobs.module';
-import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Application.name, schema: ApplicationSchema }]),
+    TypeOrmModule.forFeature([Application]),
     JobsModule,
-    NotificationsModule,
   ],
   controllers: [ApplicationsController],
   providers: [ApplicationsService],
   exports: [ApplicationsService],
 })
-export class ApplicationsModule {}
+export class ApplicationsModule { }
